51e0282a55ef772994c26262a52eca9436361041
pcoleman <pcoleman@geotogether.com>
2024-05-26 20:01:16 +0100

    First cut of TC_DEM_2_9.py

---
+++ src/python_testing/TC_DEM_2_9.py
@@ -0,0 +1,81 @@
+#
+#    Copyright (c) 2024 Project CHIP Authors
+#    All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+# pylint: disable=invalid-name
+"""Define Matter test case TC_DEM_2_8."""
+
+
+import sys
+import logging
+import datetime
+
+import chip.clusters as Clusters
+from chip.clusters.Types import NullValue
+from chip.interaction_model import Status
+from matter_testing_support import EventChangeCallback, MatterBaseTest, TestStep, async_test_body, default_matter_test_main
+from mobly import asserts
+from TC_DEM_Utils import DEMBaseTestHelper
+
+logger = logging.getLogger(__name__)
+
+
+class TC_DEM_2_8(MatterBaseTest, DEMBaseTestHelper):
+    """Implementation of test case TC_DEM_2_8."""
+
+    def desc_TC_DEM_2_8(self) -> str:
+        """Return a description of this test."""
+        return "4.1.3. [TC-DEM-2.2] Power Adjustment feature functionality with DUT as Server"
+
+    def pics_TC_DEM_2_8(self):
+        """Return the PICS definitions associated with this test."""
+        pics = [
+            "DEM.S.F00",  # Depends on Feature 00 (PowerAdjustment)
+        ]
+        return pics
+
+    def steps_TC_DEM_2_8(self) -> list[TestStep]:
+        """Execute the test steps."""
+        steps = [
+            TestStep("1", "Commissioning, already done. "),
+            TestStep("2", "TH reads TestEventTriggersEnabled attribute from General Diagnostics Cluster. Verify that TestEventTriggersEnabled attribute has a value of 1 (True)"),
+            TestStep("3", "TH sends TestEventTrigger command for Forecast Test Event. "),
+            TestStep("3a", "TH reads Forecast. Value has to include a valid slots[0].ManufacturerESAState"),
+            TestStep("3b", "TH reads Forecast. Value has to include valid slots[0].NominalPower, slots[0].MinPower, slots[0].MaxPower, slots[0].NominalEnergy"),
+            TestStep("4", "TH sends TestEventTrigger command for Forecast Test Event Clear. "),
+        ]
+
+        return steps
+
+    @async_test_body
+    async def test_TC_DEM_2_8(self):
+        # pylint: disable=too-many-locals, too-many-statements
+        """Run the test steps."""
+        self.step("1")
+        # Commission DUT - already done
+
+        # Subscribe to Events and when they are sent push them to a queue for checking later
+        events_callback = EventChangeCallback(Clusters.DeviceEnergyManagement)
+        await events_callback.start(self.default_controller,
+                                    self.dut_node_id,
+                                    self.matter_test_config.endpoint)
+
+
+
+        self.step("2")
+        await self.check_test_event_triggers_enabled()
+
+
+if __name__ == "__main__":
+    default_matter_test_main()
